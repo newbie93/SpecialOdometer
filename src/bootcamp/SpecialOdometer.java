a5759311fc62274dc1c63afa5a7ae6928058d2ac
Ghosh <righosh@lm-maa-22004976.paypalcorp.com>
2018-08-09 13:46:08 +0530

    complete ver 1

--- src/bootcamp/SpecialOdometer.java
+++ src/bootcamp/SpecialOdometer.java
@@ -3,31 +3,98 @@ package bootcamp;
 import java.util.ArrayList;
 
 public class SpecialOdometer {
-	
+
 	private int numOfDigits;
 	private ArrayList<String> validDistances;
-	
+
 	public SpecialOdometer(int numOfDigits) {
+		init(numOfDigits);
+	}
+
+	private void init(int numOfDigits) {
 		this.numOfDigits=numOfDigits;
 		validDistances=new ArrayList<>();
 		generateValidDistances("", '1', 0);
-		//for(String str:validDistances)
-		//	System.out.println(str);
+		//displayAllValidDistances();
 	}
-	
+
+	private void displayAllValidDistances() {
+		for(String str:validDistances)
+			System.out.println(str);
+	}
+
+	public boolean isValidDistance(String dist) {
+		return validDistances.contains(dist);
+	}
+
 	private void generateValidDistances(String str,char startingDigit, int num) {
 		if(num==this.numOfDigits) {
 			validDistances.add(str);
 			return;
 		}
-		while(startingDigit<='9')
-			generateValidDistances(str+startingDigit,(startingDigit=(char)(startingDigit+1)),num+1);
+		while(startingDigit<='9')	{
+			generateValidDistances(str+startingDigit,(char)(startingDigit+1),num+1);
+			startingDigit=(char)(startingDigit+1);
+		}
+	}
+
+	public int DifferenceOfDistances(String distance1, String distance2) throws DistanceException {
+		if(!isValidDistance(distance1) || !isValidDistance(distance2))
+			throw new DistanceException();
+		return Math.abs(validDistances.indexOf(distance1) - validDistances.indexOf(distance2));
+	}
+
+	public String nextReading(String reading) throws DistanceException {
+		if(!isValidDistance(reading))
+			throw new DistanceException();
+		return nextNthReading(reading, 1);
+	}
+
+
+
+	public String prevReading(String reading) throws DistanceException {
+		if(!isValidDistance(reading))
+			throw new DistanceException();
+		return prevNthReading(reading, 1);
+	}
+
+
+
+	public String nextNthReading(String reading, int n) throws DistanceException {
+		if(!isValidDistance(reading))
+			throw new DistanceException();
+		int index = validDistances.indexOf(reading);
+		int resIndex = (index+n)%validDistances.size();
+		return validDistances.get(resIndex);
+	}
+
+
+
+	public String prevNthReading(String reading, int n) throws DistanceException {
+		if(!isValidDistance(reading))
+			throw new DistanceException();
+		int index = validDistances.indexOf(reading);
+		int resIndex = (validDistances.size()+index-(n%validDistances.size()))%validDistances.size();
+		return validDistances.get(resIndex);
+	}
+
+}
+
+class DistanceException extends Exception {
+	private final static String msg="Distance is not valid!";
+	@Override
+	public String toString() {
+		return this.msg;
 	}
 }
 
 class Main {
-	public static void main(String a[]) {
-			//ystem.out.println(x);
+	public static void main(String a[]) throws Exception {
 		SpecialOdometer specialOdometer=new SpecialOdometer(8);
+		System.out.println(specialOdometer.DifferenceOfDistances("12356789","12345678"));
+		System.out.println(specialOdometer.nextReading("23456789"));
+		System.out.println(specialOdometer.prevReading("12345678"));
+		System.out.println(specialOdometer.nextNthReading("23456789",10));
+		System.out.println(specialOdometer.prevNthReading("12345678",10));
 	}
 }
