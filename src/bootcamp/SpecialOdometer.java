e049df32bafe52a9bc2a84ed59e68401e48bbce3
Ghosh <righosh@lm-maa-22004976.paypalcorp.com>
2018-08-09 13:19:58 +0530

    generating valid distances

--- src/bootcamp/SpecialOdometer.java
+++ src/bootcamp/SpecialOdometer.java
@@ -5,13 +5,29 @@ import java.util.ArrayList;
 public class SpecialOdometer {
 	
 	private int numOfDigits;
-	private ArrayList<String>validDistances;
+	private ArrayList<String> validDistances;
 	
 	public SpecialOdometer(int numOfDigits) {
 		this.numOfDigits=numOfDigits;
 		validDistances=new ArrayList<>();
+		generateValidDistances("", '1', 0);
+		//for(String str:validDistances)
+		//	System.out.println(str);
 	}
 	
-	
+	private void generateValidDistances(String str,char startingDigit, int num) {
+		if(num==this.numOfDigits) {
+			validDistances.add(str);
+			return;
+		}
+		while(startingDigit<='9')
+			generateValidDistances(str+startingDigit,(startingDigit=(char)(startingDigit+1)),num+1);
+	}
+}
 
+class Main {
+	public static void main(String a[]) {
+			//ystem.out.println(x);
+		SpecialOdometer specialOdometer=new SpecialOdometer(8);
+	}
 }
