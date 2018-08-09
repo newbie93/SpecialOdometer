18bf4f5c3fb4bc5e51c7f77fdadb565c4a41f560
Ghosh <righosh@lm-maa-22004976.paypalcorp.com>
2018-08-09 12:09:35 +0530

    class SpecialOdometer start

---
+++ src/bootcamp/SpecialOdometer.java
@@ -0,0 +1,17 @@
+package bootcamp;
+
+import java.util.ArrayList;
+
+public class SpecialOdometer {
+	
+	private int numOfDigits;
+	private ArrayList<String>validDistances;
+	
+	public SpecialOdometer(int numOfDigits) {
+		this.numOfDigits=numOfDigits;
+		validDistances=new ArrayList<>();
+	}
+	
+	
+
+}
